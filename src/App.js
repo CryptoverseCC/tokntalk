425169b44965d9cdbb3d631a47dc1a975b81ff93
Michał Warda <t1du53k@gmail.com>
2018-03-21 11:47:41 +0100

    Extract components

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const purrs = [
     created_at: 1520425637000,
     id: 'claim:0x3e58d7a2955b3677fcb63752b906640c04bc9d773c07c11d146955cd7edc4ead:0',
     message: "I'm supercat! (true story)",
-    sequence: 5212559,
+    sequence: 5212666,
     token_id: '341605',
     family: 'ethereum'
   },
@@ -23,7 +23,7 @@ const purrs = [
     created_at: 1520425637000,
     id: 'claim:0x3e58d7a2955b3677fcb63752b906640c04bc9d773c07c11d146955cd7edc4ead:0',
     message: "I'm supercat! (true story)",
-    sequence: 5212559,
+    sequence: 5212665,
     token_id: '341605',
     family: 'ethereum'
   },
@@ -32,7 +32,7 @@ const purrs = [
     created_at: 1520425637000,
     id: 'claim:0x3e58d7a2955b3677fcb63752b906640c04bc9d773c07c11d146955cd7edc4ead:0',
     message: "I'm supercat! (true story)",
-    sequence: 5212559,
+    sequence: 5212664,
     token_id: '341605',
     family: 'ethereum'
   },
@@ -41,7 +41,7 @@ const purrs = [
     created_at: 1520425637000,
     id: 'claim:0x3e58d7a2955b3677fcb63752b906640c04bc9d773c07c11d146955cd7edc4ead:0',
     message: "I'm supercat! (true story)",
-    sequence: 5212559,
+    sequence: 5212663,
     token_id: '341603',
     family: 'ethereum'
   },
@@ -50,7 +50,7 @@ const purrs = [
     created_at: 1520425637000,
     id: 'claim:0x3e58d7a2955b3677fcb63752b906640c04bc9d773c07c11d146955cd7edc4ead:0',
     message: "I'm supercat! (true story)",
-    sequence: 5212559,
+    sequence: 5212662,
     token_id: '341605',
     family: 'ethereum'
   },
@@ -59,7 +59,7 @@ const purrs = [
     created_at: 1520425637000,
     id: 'claim:0x3e58d7a2955b3677fcb63752b906640c04bc9d773c07c11d146955cd7edc4ead:0',
     message: "I'm supercat! (true story)",
-    sequence: 5212559,
+    sequence: 5212661,
     token_id: '341605',
     family: 'ethereum'
   },
@@ -68,7 +68,7 @@ const purrs = [
     created_at: 1520425637000,
     id: 'claim:0x3e58d7a2955b3677fcb63752b906640c04bc9d773c07c11d146955cd7edc4ead:0',
     message: "I'm supercat! (true story)",
-    sequence: 5212559,
+    sequence: 5212660,
     token_id: '341605',
     family: 'ethereum'
   }
@@ -147,161 +147,147 @@ const PurrGroup = ({ catId, img, children }) => (
   </div>
 );
 
+const ShowCat = ({ match: { params: { catId } } }) => (
+  <React.Fragment>
+    <section className="hero hero-kitten is-small">
+      <div className="hero-body">
+        <div className="columns">
+          <div className="column is-12 has-text-centered">
+            <div className="your-kitten">
+              <img style={{ width: '450px', height: 'auto' }} src={kitten} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section style={{ paddingTop: '4rem' }}>
+      <div className="container">
+        {transformPurrsToPurrGroups(purrs.filter(purr => purr.token_id === catId)).map(
+          ({ catId, purrs }, groupIndex) => (
+            <PurrGroup key={groupIndex} catId={catId} img={kitty1}>
+              {purrs.map(({ message, sequence }, purrIndex) => (
+                <Purr key={purrIndex} message={message} date={sequence} />
+              ))}
+            </PurrGroup>
+          )
+        )}
+      </div>
+    </section>
+  </React.Fragment>
+);
+
+const Index = () => (
+  <React.Fragment>
+    <section className="hero">
+      <div className="hero-body">
+        <div className="container">
+          <div className="columns">
+            <div className="column is-9 is-offset-3">
+              <h1 className="title txtwav slow">
+                <SplitString>Purr Purr</SplitString>
+              </h1>
+              <div className="subtitle">
+                <h2 className="txtwav slow">
+                  <SplitString>Make your cryptokitten talk with</SplitString>
+                </h2>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section style={{ paddingTop: '4rem' }}>
+      <div className="container">
+        <PurrGroup catId="189402" img={kitty1}>
+          <PurrForm />
+        </PurrGroup>
+        {transformPurrsToPurrGroups(purrs).map(({ catId, purrs }, groupIndex) => (
+          <PurrGroup key={groupIndex} catId={catId} img={kitty1}>
+            {purrs.map(({ message, sequence }, purrIndex) => (
+              <Purr key={purrIndex} message={message} date={sequence} />
+            ))}
+          </PurrGroup>
+        ))}
+      </div>
+    </section>
+  </React.Fragment>
+);
+
+const Navigation = () => (
+  <nav className="navigation">
+    <div className="container">
+      <div className="columns" style={{ alignItems: 'center' }}>
+        <div className="column is-2">
+          <Link to="/">
+            <h3 className="logo">Purrbook</h3>
+          </Link>
+        </div>
+        <div className="column is-10 has-text-right">
+          <div className="select">
+            <select id="account">
+              <option>Kitten #189402</option>
+            </select>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+);
+
+const Footer = () => (
+  <footer className="fixed_footer">
+    <div className="container">
+      <div className="columns">
+        <div className="column is-offset-3">
+          <h3 className="is-size-2">Every address is a feed. Interested?</h3>
+        </div>
+      </div>
+      <div className="columns">
+        <div className="column is-offset-3">
+          <a className="external" href="https://linkexchange.io" target="_blank">
+            <div className="columns">
+              <div className="column is-1 arrow">
+                <img style={{ width: '16px', height: 'auto', marginTop: '10px' }} src={arrow} />
+              </div>
+              <div className="column">
+                <h2>Brought by Link Exchange</h2>
+                <p>"It's like Google AdSense for Token-based Communities"</p>
+              </div>
+            </div>
+          </a>
+        </div>
+        <div className="column">
+          <a className="external" href="https://userfeeds.io" target="_blank">
+            <div className="columns">
+              <div className="column is-1 arrow">
+                <img style={{ width: '16px', height: 'auto', marginTop: '10px' }} src={arrow} />
+              </div>
+
+              <div className="column">
+                <h2>Powered by Userfeeds</h2>
+                <p>Attention Economies for Token-Based Communities</p>
+              </div>
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  </footer>
+);
+
 class App extends Component {
   render() {
     return (
       <Router>
         <React.Fragment>
           <div className="main">
-            <nav className="navigation">
-              <div className="container">
-                <div className="columns" style={{ alignItems: 'center' }}>
-                  <div className="column is-2">
-                    <Link to="/">
-                      <h3 className="logo">Purrbook</h3>
-                    </Link>
-                  </div>
-                  <div className="column is-10 has-text-right">
-                    <div className="select">
-                      <select id="account">
-                        <option>Kitten #189402</option>
-                      </select>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </nav>
+            <Navigation />
             <Switch>
-              <Route exact path="/:catId">
-                <React.Fragment>
-                  <section className="hero hero-kitten is-small">
-                    <div className="hero-body">
-                      <div className="columns">
-                        <div className="column is-12 has-text-centered">
-                          <div className="your-kitten">
-                            <img style={{ width: '450px', height: 'auto' }} src={kitten} />
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </section>
-                  <section style={{ paddingTop: '4rem' }}>
-                    <div className="container">
-                      <PurrGroup catId="189402" img={kitty1}>
-                        <PurrForm />
-                        <Purr message={'Teddy bears taking over!'} date={'10/12/2018'} />
-                        <Purr
-                          message={'A thank you, to everyone working countless hours to solve the scaling problem'}
-                          date={'09/12/2018'}
-                        />
-                        <Purr
-                          message={
-                            'Anyone interested to be the iOS track judge for @it_challenges this weekend in Krakow? #iosdev https://itchallenges.me/'
-                          }
-                          date={'10/12/2018'}
-                        />
-                        <Purr
-                          message={
-                            '#DeleteFacebook Movement Gains Steam After 50 Million Users Have Data Leaked $FB https://goo.gl/LA9wRb'
-                          }
-                          date={'09/12/2018'}
-                        />
-                        <Purr
-                          message={
-                            "I deleted my Facebook account over a year ago and noticed a very real drop in anxiety. I have never been a particularly anxious person and until then, hadn't realised how deeply entrenched I had become in what is essentially an addictive game with shit graphics. #DeleteFacebook"
-                          }
-                          date={'10/12/2018'}
-                        />
-                        <Purr
-                          message={
-                            'There is a pleasure in righteous fury. I savor it about silly things, like lids left ajar or autocorrect insisting “lids” is “kids” So that I can be kind about things that matter.'
-                          }
-                          date={'09/12/2018'}
-                        />
-                      </PurrGroup>
-                    </div>
-                  </section>
-                </React.Fragment>
-              </Route>
-              <Route exact path="/">
-                <React.Fragment>
-                  <section className="hero">
-                    <div className="hero-body">
-                      <div className="container">
-                        <div className="columns">
-                          <div className="column is-9 is-offset-3">
-                            <h1 className="title txtwav slow">
-                              <SplitString>Purr Purr</SplitString>
-                            </h1>
-                            <div className="subtitle">
-                              <h2 className="txtwav slow">
-                                <SplitString>Make your cryptokitten talk with</SplitString>
-                              </h2>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </section>
-                  <section style={{ paddingTop: '4rem' }}>
-                    <div className="container">
-                      <PurrGroup catId="189402" img={kitty1}>
-                        <PurrForm />
-                      </PurrGroup>
-                      {transformPurrsToPurrGroups(purrs).map(({ catId, purrs }, groupIndex) => (
-                        <PurrGroup key={groupIndex} catId={catId} img={kitty1}>
-                          {purrs.map(({ message, sequence }, purrIndex) => (
-                            <Purr key={purrIndex} message={message} date={sequence} />
-                          ))}
-                        </PurrGroup>
-                      ))}
-                    </div>
-                  </section>
-                </React.Fragment>
-              </Route>
+              <Route exact path="/:catId" component={ShowCat} />
+              <Route exact path="/" component={Index} />
             </Switch>
           </div>
-          <footer className="fixed_footer">
-            <div className="container">
-              <div className="columns">
-                <div className="column is-offset-3">
-                  <h3 className="is-size-2">Every address is a feed. Interested?</h3>
-                </div>
-              </div>
-
-              <div className="columns">
-                <div className="column is-offset-3">
-                  <a className="external" href="https://linkexchange.io" target="_blank">
-                    <div className="columns">
-                      <div className="column is-1 arrow">
-                        <img style={{ width: '16px', height: 'auto', marginTop: '10px' }} src={arrow} />
-                      </div>
-
-                      <div className="column">
-                        <h2>Brought by Link Exchange</h2>
-                        <p>"It's like Google AdSense for Token-based Communities"</p>
-                      </div>
-                    </div>
-                  </a>
-                </div>
-
-                <div className="column">
-                  <a className="external" href="https://userfeeds.io" target="_blank">
-                    <div className="columns">
-                      <div className="column is-1 arrow">
-                        <img style={{ width: '16px', height: 'auto', marginTop: '10px' }} src={arrow} />
-                      </div>
-
-                      <div className="column">
-                        <h2>Powered by Userfeeds</h2>
-                        <p>Attention Economies for Token-Based Communities</p>
-                      </div>
-                    </div>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </footer>
+          <Footer />
         </React.Fragment>
       </Router>
     );
