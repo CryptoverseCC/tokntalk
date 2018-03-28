3d2d3a40c19c89215aa928041ff51b9fd3b370c3
Michał Warda <t1du53k@gmail.com>
2018-03-28 11:17:33 +0200

    Add no identity detected.

--- src/stories/index.js
+++ src/stories/index.js
@@ -4,32 +4,38 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
-const Header = ({status}) => {
+const Header = ({ status }) => {
   return (
     <div
       className="level"
       style={{
         height: '65px',
         backgroundColor: '#f9fbfd',
-        padding: '0 1rem',
         borderBottom: '1px solid #e8e8f1',
         fontFamily: 'Rubik',
         fontSize: '14px',
         fontWeight: '500'
       }}
     >
-      <div className="level-left">Feed</div>
-      <h1 className="level-item" style={{ color: '#1B2437', fontWeight: 'inherit', fontSize: 'inherit', lineHeight: 'inherit' }}>
-        Purrbook
-      </h1>
-      <div
-        className="level-right has-text-right"
-        style={{ color: '#FC0035', textShadow: '0 0 10px rgba(252,0,53,0.3)' }}
-      >
-        No Metamask
+      <div className="container is-fluid level-item level columns">
+        <div className="level-left column">Feed</div>
+        <h1
+          className="level-item column has-text-centered"
+          style={{ color: '#1B2437', fontWeight: 'inherit', fontSize: 'inherit', lineHeight: 'inherit' }}
+        >
+          Purrbook
+        </h1>
+        <div
+          className="level-right has-text-right column"
+          style={{ color: '#FC0035', textShadow: '0 0 10px rgba(252,0,53,0.3)' }}
+        >
+          {status}
+        </div>
       </div>
     </div>
   );
 };
 
-storiesOf('NewHeader', module).add('No Metamask', () => <Header />);
+storiesOf('NewHeader', module)
+  .add('No Metamask', () => <Header status="No Metamask" />)
+  .add('No identity detected', () => <Header status="No identity detected" />);
