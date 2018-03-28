78c30d8321f6e218bf74bc818f4f9af13e7c5d82
Michał Warda <t1du53k@gmail.com>
2018-03-28 12:17:53 +0200

    Add avatar to identity status.

--- src/stories/index.js
+++ src/stories/index.js
@@ -5,8 +5,33 @@ import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
 const IdentityStatus = ({ id }) => (
-  <div className="level-right has-text-right column" style={{ color: '#1B2437' }}>
-    {id}
+  <div className="level-right column" style={{ color: '#1B2437' }}>
+    <div className="level-right">
+      <div
+        style={{
+          overflow: 'hidden',
+          width: '44px',
+          height: '44px',
+          position: 'relative',
+          borderRadius: '50%',
+          backgroundColor: '#CDF5D4'
+        }}
+      >
+        <img
+          style={{
+            width: '110px',
+            position: 'absolute',
+            left: '55%',
+            top: '85%',
+            transform: 'translate(-50%, -50%)',
+            maxWidth: 'none'
+          }}
+          alt=""
+          src="https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/635286.svg"
+        />
+      </div>
+      <div style={{ marginLeft: '8px' }}>{id}</div>
+    </div>
   </div>
 );
 
@@ -47,7 +72,7 @@ const Header = ({ status }) => {
 };
 
 storiesOf('NewHeader', module)
-  .add('No Metamask', () => <Header status={<ErrorStatus message={"No Metamask"} />} />)
-  .add('No identity detected', () => <Header status={<ErrorStatus message={"No identity detected"} />} />)
+  .add('No Metamask', () => <Header status={<ErrorStatus message={'No Metamask'} />} />)
+  .add('No identity detected', () => <Header status={<ErrorStatus message={'No identity detected'} />} />)
   .add('Metamask locked', () => <Header status={<ErrorStatus message={'Metamask locked'} />} />)
   .add('with identity', () => <Header status={<IdentityStatus id={'Cpt. Barbossa'} />} />);
