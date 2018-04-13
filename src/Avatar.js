48394485a29ca07d0c693dbad45368202df019d5
Michał Warda <t1du53k@gmail.com>
2018-04-13 18:03:13 +0200

    Add styled components and start bringing back storybook

--- src/Avatar.js
+++ src/Avatar.js
@@ -1,56 +1,46 @@
 import React from 'react';
-import LazyLoad from 'react-lazyload';
-import { avatarSizes } from "./entityApi";
+import { avatarSizes } from './entityApi';
+import styled, { css } from 'styled-components';
+
+const AvatarContainer = styled.div`
+  overflow: hidden;
+  position: relative;
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  ${({ backgroundColor, containerSize }) => css`
+    background-color: ${backgroundColor};
+    width: ${containerSize};
+    height: ${containerSize};
+  `};
+`;
+
+const Avatar = styled.img`
+  position: absolute;
+  transform: translate(-50%, -50%);
+  max-width: none;
+  ${({ imgSize, imgLeftOffset, imgTopOffset }) => css`
+    left: ${imgLeftOffset};
+    top: ${imgTopOffset};
+    width: ${imgSize};
+  `};
+`;
+
+const ReactionContainer = styled.div`
+  position: absolute;
+  bottom: 0;
+  left: 50%;
+  transform: translate(-50%, 50%);
+`;
 
 const IdentityAvatar = ({ size, reaction, style = {}, backgroundColor, src, lazy = true }) => {
   const { containerSize, imgSize, imgTopOffset, imgLeftOffset } = avatarSizes[size];
   return (
     <div style={{ position: 'relative', ...style }}>
-      <div
-        style={{
-          overflow: 'hidden',
-          position: 'relative',
-          width: containerSize,
-          height: containerSize,
-          borderRadius: '50%',
-          backgroundColor
-        }}
-      >
-        {lazy ? (
-          <LazyLoad height={containerSize} once>
-            <img
-              style={{
-                width: imgSize,
-                position: 'absolute',
-                left: imgLeftOffset,
-                top: imgTopOffset,
-                transform: 'translate(-50%, -50%)',
-                maxWidth: 'none'
-              }}
-              alt=""
-              src={src}
-            />
-          </LazyLoad>
-        ) : (
-          <img
-            style={{
-              width: imgSize,
-              position: 'absolute',
-              left: imgLeftOffset,
-              top: imgTopOffset,
-              transform: 'translate(-50%, -50%)',
-              maxWidth: 'none'
-            }}
-            alt=""
-            src={src}
-          />
-        )}
-      </div>
-      {reaction && (
-        <div style={{ position: 'absolute', bottom: 0, left: '50%', transform: 'translate(-50%, 50%)' }}>
-          {reaction}
-        </div>
-      )}
+      <AvatarContainer backgroundColor={backgroundColor} containerSize={containerSize}>
+        <Avatar src={src} imgSize={imgSize} imgTopOffset={imgTopOffset} imgLeftOffset={imgLeftOffset} />
+      </AvatarContainer>
+      {reaction && <ReactionContainer>{reaction}</ReactionContainer>}
     </div>
   );
 };
