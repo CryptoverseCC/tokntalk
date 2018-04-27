4f5e0bd2311f1056e95d2dd6cced2972a51f3b20
Jakub Stasiak <kubafr2@gmail.com>
2018-04-27 10:17:07 +0200

    Dev: add env for codeship

--- release.sh
+++ release.sh
@@ -6,4 +6,4 @@ echo 'REACT_APP_INTERFACE_VALUE="https://cryptopurr.co"
 REACT_APP_BASENAME="/"' > .env.local
 
 yarn build
-yarn firebase deploy -P default --token $FIREBASE_TOKEN --message \"Release $CI_COMMIT_MESSAGE $CI_COMMIT_ID\"
+yarn firebase deploy -P default --token $FIREBASE_TOKEN --message "$CI_COMMIT_ID"
