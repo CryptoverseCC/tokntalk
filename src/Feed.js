99599f8c389006aef6e922ea533a496f729d225c
Jakub Stasiak <kubafr2@gmail.com>
2018-07-11 15:13:05 +0200

    links regex

--- src/Feed.js
+++ src/Feed.js
@@ -89,7 +89,7 @@ const Label = ({ onClick, liked, count }) => {
 };
 
 export const sanitizeMessage = (message) => {
-  const expression = /(https?:\/\/[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,7}\b(?:[-a-zA-Z0-9@;:%_+.,~#?&//=]*))/g;
+  const expression = /(https?:\/\/[^.,?!:;\s]+(?:[.,?!:;]+[^.,?!:;\s]+)+)/g;
   const replaceMatchWithLink = (match) => {
     return `<a href="${match}">${escapeHtml(match)}</a>`;
   };
