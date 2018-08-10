f248a8493b9f890909599f722d66b6edc528075e
ghostbuster91 <kghost0@gmail.com>
2018-08-10 10:39:15 +0200

    Add bare modal for verification info

--- src/Feed.js
+++ src/Feed.js
@@ -457,6 +457,9 @@ export class Card extends React.Component {
   };
 
   showMoreReplies = () => this.setState({ areRepliesCollapsed: false });
+  onVerify = () => {
+    this.setState({ showVerify: true });
+  };
 
   onVerify = (feedItem) => {
     this.setState({ showVerify: true, verifiableItem: feedItem });
