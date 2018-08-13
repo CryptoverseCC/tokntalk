da2e52437de74c6c057290dca219c5e958d69f0d
patryk adaś <patryk.adas@gmail.com>
2018-08-13 11:41:46 +0200

    Revert "small css improvements"
    This reverts commit d9ee00f9beb4312824d4d1928899d873db1c1065.

--- src/Discover.js
+++ src/Discover.js
@@ -19,10 +19,10 @@ import { ExclamationMark } from './Icons';
 import ercs20, { TokenImage } from './erc20';
 import { ConnectedClubForm, CommentForm } from './CommentForm';
 import { hasValidContext, getRanking, isValidFeedItem, enhanceFeedItem } from './api';
-import { socialIcons } from './Icons';
+import AddToken from './AddToken';
+import { socialIcons, AddIcon } from './Icons';
 import { FlatContainer, WarningContainer, H1, H2, H3, H4, SocialUsername, ContentContainer } from './Components';
 import {
-  EntityName,
   LinkedEntityAvatar,
   IfActiveEntity,
   IfActiveEntityIs,
@@ -35,7 +35,7 @@ import {
 import exportIcon from './img/export.svg';
 
 const H1Discover = styled.h1`
-  margin: 60px 0 20px 0;
+  margin: 60px 0;
   font-size: 4rem;
   font-weight: bold;
   line-height: 1.1;
@@ -108,15 +108,23 @@ class Index extends Component {
     }
   }
 
-  renderEntityTokens = (entity) =>
-    this.renderTiles(
-      entity.tokens.map((asset) => {
-        const [network, address] = asset.split(':');
-        return find({ network, address })(ercs20);
-      }),
-    );
+  renderEntityTokens = (entity) => (
+    <div className="columns is-multiline">
+      {this.renderTiles(
+        entity.tokens.map((asset) => {
+          const [network, address] = asset.split(':');
+          return find({ network, address })(ercs20);
+        }),
+      )}
+    </div>
+  );
 
-  renderOthersTokens = () => this.renderTiles(this.state.loading ? [] : this.sortByScore());
+  renderOthersTokens = () => (
+    <div className="columns is-multiline">
+      <AddToken className="column is-one-quarter" />
+      {this.renderTiles(this.state.loading ? [] : this.sortByScore())}
+    </div>
+  );
 
   sortByScore = () => {
     const tokensMap = ercs20.reduce((acc, item) => ({ ...acc, [`${item.network}:${item.address}`]: item }), {});
@@ -126,17 +134,16 @@ class Index extends Component {
   renderTiles = (tokens) => {
     const { match } = this.props;
     return (
-      <div className="columns is-multiline">
+      <React.Fragment>
         {tokens.map((token) => (
           <TokenTile
             linkTo={`${match.url}/byToken/${token.symbol}`}
             key={token.address}
             token={token}
             className="column is-one-quarter"
-            style={{ padding: '1rem' }}
           />
         ))}
-      </div>
+      </React.Fragment>
     );
   };
 
@@ -811,18 +818,19 @@ const TokenTileCotainer = styled.div`
   padding-top: 105%;
   border-radius: 12px;
   overflow: hidden;
-  transition: all 0.15s ease-in;
+  transition: transform 0.3s;
+  transition: all 0.15s ease;
 
   :hover {
-    transform: translateY(-2px);
-    box-shadow: ${({ shadowColor, small }) => !small && `0 3rem 4rem -2rem  ${shadowColor}`};
-    transition: all 0.15s ease-out;
+    transform: translateY(-3px);
+    box-shadow: ${({ shadowColor, small }) => !small && `0 3rem 6rem -2rem  ${shadowColor}`};
+    transition: all 0.15s ease;
   }
 
   :active {
     transform: scale(0.98);
     box-shadow: ${({ shadowColor, small }) => !small && `0 3rem 4rem -2rem  ${shadowColor}`};
-    transitionn: all 0.15s ease-in;
+    transitionn: all 0.15s ease;
   }
 
   @media (max-width: 770px) {
