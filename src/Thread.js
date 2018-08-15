5fcde3b0fee8073ddeeb22c3f42558f9ea43fd22
Jakub Stasiak <kubafr2@gmail.com>
2018-08-15 11:23:48 +0200

    Links to main threads

--- src/Thread.js
+++ src/Thread.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 
 import { FixedModal } from './Modal';
 import { Card, LikersModal } from './Feed';
@@ -7,7 +8,7 @@ import Context from './Context';
 import { HeaderSpacer } from './Header';
 import { ContentContainer } from './Components';
 
-export class Thread extends Component {
+class ThreadCmp extends Component {
   state = {
     showModal: false,
     feedItemLikes: [],
@@ -26,40 +27,52 @@ export class Thread extends Component {
 
     return (
       <React.Fragment>
-        <HeaderSpacer />
-        <ContentContainer>
-          <Context.Consumer>
-            {({ feedStore: { feedItemLoading, feedItem, temporaryReactions } }) => {
-              const getTemporaryReactions = (id) => temporaryReactions[id] || [];
+        <Context.Consumer>
+          {({ feedStore: { feedItemLoading, feedItem, temporaryReactions } }) => {
+            const getTemporaryReactions = (id) => temporaryReactions[id] || [];
 
-              return !!feedItem || (!feedItemLoading && !!feedItem) ? (
-                <Card
-                  feedItem={feedItem}
-                  replies={feedItem.replies}
-                  reactions={feedItem.likes}
-                  style={{ background: '#ffffff' }}
-                  onShowLikers={this.onShowLikers}
-                  getTemporaryReactions={getTemporaryReactions}
-                />
-              ) : (
-                <div
-                  style={{
-                    paddingTop: '20px',
-                    display: 'flex',
-                    justifyContent: 'center',
-                  }}
-                >
-                  <Loader />
-                </div>
-              );
-            }}
-          </Context.Consumer>
-        </ContentContainer>
+            return !!feedItem || (!feedItemLoading && !!feedItem) ? (
+              <Card
+                feedItem={feedItem}
+                replies={feedItem.replies}
+                reactions={feedItem.likes}
+                style={{ background: '#ffffff' }}
+                onShowLikers={this.onShowLikers}
+                getTemporaryReactions={getTemporaryReactions}
+              />
+            ) : (
+              <div
+                style={{
+                  paddingTop: '20px',
+                  display: 'flex',
+                  justifyContent: 'center',
+                }}
+              >
+                <Loader />
+              </div>
+            );
+          }}
+        </Context.Consumer>
         {showModal && <LikersModal onClose={() => this.setState({ showModal: false })} likes={feedItemLikes} />}
       </React.Fragment>
     );
   }
 }
+
+export const Thread = (props) => (
+  <React.Fragment>
+    <HeaderSpacer style={{ marginBottom: '60px' }} />
+    <ContentContainer>
+      <ThreadCmp {...props} />
+    </ContentContainer>
+  </React.Fragment>
+);
+
+const ModalContainer = styled.div`
+  max-height: 90vh;
+  overflow-y: scroll;
+  border-radius: 12px;
+`;
 
 export class ModalThread extends Component {
   onClose = () => {
@@ -69,7 +82,9 @@ export class ModalThread extends Component {
   render() {
     return (
       <FixedModal onClose={this.onClose}>
-        <Thread {...this.props} />
+        <ModalContainer>
+          <ThreadCmp {...this.props} />
+        </ModalContainer>
       </FixedModal>
     );
   }
