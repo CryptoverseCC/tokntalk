59a659dad995862e075b840d50d63ef8aeab0f8d
patryk adaś <patryk.adas@gmail.com>
2018-08-15 19:29:21 +0200

    Starting landing page
    Overall structure, typography

--- src/Landing.js
+++ src/Landing.js
@@ -1,28 +1,180 @@
 import React, { Component } from 'react';
 import css from './css/landing.css';
 import styled from 'styled-components';
+import Logo from './Logo';
 
 const Landing = styled((props) => (
-  <div className="main">
-    <h1>I ambig ugly HTML monster</h1>
-    <p>
-      Aliqua aute deserunt labore id consequat id qui ex aliquip elit ex nulla dolore sed sit veniam sunt amet nulla
-      proident in dolor pariatur consectetur quis proident qui dolore nisi et esse enim anim duis minim id cillum velit
-      dolore ex ut aliqua ullamco esse ut magna nisi elit cupidatat nisi esse esse duis proident minim ea incididunt
-      nulla consequat officia magna dolor consequat fugiat in laboris quis sit ea et id commodo aute voluptate minim id
-      aliquip non ut dolor culpa sed occaecat sit eu esse enim anim aliqua est est culpa incididunt ea et laboris
-      exercitation sunt est enim officia non quis laboris cillum est sunt officia duis exercitation commodo minim
-      laborum aliquip eiusmod dolore incididunt dolor incididunt sint ex in dolore ea officia aliqua eiusmod ea nisi
-      exercitation commodo consectetur officia culpa irure officia nulla occaecat enim consequat aliqua in culpa veniam
-      laboris sit eu aliqua nisi cillum est in non nulla labore exercitation magna irure reprehenderit deserunt et
-      nostrud velit do exercitation minim enim cupidatat eiusmod ea est pariatur adipisicing sit eu dolor ea proident in
-      elit ut anim dolor sed exercitation tempor velit labore magna non ut incididunt labore cillum exercitation quis
-      nisi in deserunt cupidatat sit occaecat in quis tempor dolor cillum do in officia tempor laborum proident sunt
-      consectetur excepteur aliqua eiusmod laboris anim amet dolore irure duis ut duis deserunt dolor fugiat laboris
-      elit eiusmod fugiat sed veniam enim cillum do do qui consectetur dolore reprehenderit cupidatat ut esse do dolore
-      duis.
-    </p>
-  </div>
+  <React.Fragment>
+    <div class="wrapper-landing">
+      <div class="container">
+        <nav class="level">
+          <div class="level-left">
+            <div class="level-item">
+              <Logo />
+              <p>Tok talk</p>
+            </div>
+          </div>
+          <div class="level-right">
+            <p class="level-item">
+              <strong>All</strong>
+            </p>
+            <p class="level-item">
+              <a>Published</a>
+            </p>
+          </div>
+        </nav>
+      </div>
+
+      <div class="container">
+        <section class="section has-text-centered landing-hero">
+          <p class="subtitle" style={{ color: '#848DA5' }}>
+            Social platform for
+          </p>
+          <h1 class="title">Tokens</h1>
+          <p class="subtitle">
+            Explore token - oriented communities. Grow your influence and earn tokens in the trusted environment.
+          </p>
+          <a class="bump" href="#">
+            Explore
+          </a>
+        </section>
+      </div>
+
+      <section
+        class="section has-text-centered"
+        style={{
+          background: 'radial-gradient(at center center, rgb(198, 191, 224) 0%, rgb(255, 255, 255) 70%)',
+          marginTop: '0',
+        }}
+      >
+        <div class="container">
+          <figure class="landing-video">
+            <p>kupa</p>
+          </figure>
+        </div>
+      </section>
+
+      <section class="section has-text-centered">
+        <div class="container">
+          <div class="landing-section-introduction">
+            <h2>Explore the reality from many angles.</h2>
+            <p class="subtitle">Discover messages from token holders for token holders.</p>
+            <a class="landing-button">Start exploring</a>
+          </div>
+          <div class="columns">
+            <div class="column is-one-third">
+              <figure class="landing-feature">
+                <img src="" />
+              </figure>
+              <h3>Yours</h3>
+              <p>Act as any Non Fungible Token (ERC721) or an address. Switch between personalities.</p>
+            </div>
+            <div class="column is-one-third">
+              <figure class="landing-feature">
+                <img src="" />
+              </figure>
+              <h3>Yours</h3>
+              <p>Act as any Non Fungible Token (ERC721) or an address. Switch between personalities.</p>
+            </div>
+            <div class="column is-one-third">
+              <figure class="landing-feature">
+                <img src="" />
+              </figure>
+              <h3>Yours</h3>
+              <p>Act as any Non Fungible Token (ERC721) or an address. Switch between personalities.</p>
+            </div>
+          </div>
+          <div class="landing-testimonial">
+            <figure class="landing-testimonial-profile">
+              <img src="" />
+            </figure>
+            <p>Something awesome</p>
+            <p class="author">Kitty</p>
+          </div>
+        </div>
+      </section>
+
+      <section class="section has-text-centered">
+        <div class="container">
+          <div class="landing-section-introduction">
+            <h2>Explore the reality from many angles.</h2>
+            <p class="subtitle">Discover messages from token holders for token holders.</p>
+            <a class="landing-button">Start exploring</a>
+          </div>
+          <div class="columns">
+            <div class="column is-one-third">
+              <figure class="landing-feature">
+                <img src="" />
+              </figure>
+              <h3>Yours</h3>
+              <p>Act as any Non Fungible Token (ERC721) or an address. Switch between personalities.</p>
+            </div>
+            <div class="column is-one-third">
+              <figure class="landing-feature">
+                <img src="" />
+              </figure>
+              <h3>Yours</h3>
+              <p>Act as any Non Fungible Token (ERC721) or an address. Switch between personalities.</p>
+            </div>
+            <div class="column is-one-third">
+              <figure class="landing-feature">
+                <img src="" />
+              </figure>
+              <h3>Yours</h3>
+              <p>Act as any Non Fungible Token (ERC721) or an address. Switch between personalities.</p>
+            </div>
+          </div>
+          <div class="landing-testimonial">
+            <figure class="landing-testimonial-profile">
+              <img src="" />
+            </figure>
+            <p>Something awesome</p>
+            <p class="author">Kitty</p>
+          </div>
+        </div>
+      </section>
+
+      <section class="section has-text-centered">
+        <div class="container">
+          <div class="landing-section-introduction">
+            <h2>Explore the reality from many angles.</h2>
+            <p class="subtitle">Discover messages from token holders for token holders.</p>
+            <a class="landing-button">Start exploring</a>
+          </div>
+          <div class="columns">
+            <div class="column is-one-third">
+              <figure class="landing-feature">
+                <img src="" />
+              </figure>
+              <h3>Yours</h3>
+              <p>Act as any Non Fungible Token (ERC721) or an address. Switch between personalities.</p>
+            </div>
+            <div class="column is-one-third">
+              <figure class="landing-feature">
+                <img src="" />
+              </figure>
+              <h3>Yours</h3>
+              <p>Act as any Non Fungible Token (ERC721) or an address. Switch between personalities.</p>
+            </div>
+            <div class="column is-one-third">
+              <figure class="landing-feature">
+                <img src="" />
+              </figure>
+              <h3>Yours</h3>
+              <p>Act as any Non Fungible Token (ERC721) or an address. Switch between personalities.</p>
+            </div>
+          </div>
+        </div>
+        <div class="landing-testimonial">
+          <figure class="landing-testimonial-profile">
+            <img src="" />
+          </figure>
+          <p>Something awesome</p>
+          <p class="author">Kitty</p>
+        </div>
+      </section>
+    </div>
+  </React.Fragment>
 ))`
   ${css};
 `;
