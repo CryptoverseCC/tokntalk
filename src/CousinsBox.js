a0167e7ee31aaa10784463a7fdfa4d461e29e3b7
ghostbuster91 <kghost0@gmail.com>
2018-09-03 12:34:58 +0200

    Use react-virtualized to lazy render cousins

--- src/CousinsBox.js
+++ src/CousinsBox.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import List from 'react-virtualized/dist/commonjs/List';
 import styled from 'styled-components';
 
 import { getEntities } from './api';
@@ -7,11 +8,7 @@ import Link from './Link';
 import { FlatContainer, H4 } from './Components';
 import { niceScroll } from './cssUtils';
 
-const CousinsList = styled.div`
-  overflow-y: scroll;
-  max-height: 300px;
-  overflow-x: hidden;
-
+const CousinsList = styled(List)`
   ${niceScroll};
 `;
 
@@ -41,16 +38,24 @@ export class CousinsBox extends Component {
       <FlatContainer style={{ marginTop: '2rem' }}>
         <H4 style={{ marginTop: '10px' }}>Cousins</H4>
         <p style={{ fontSize: '0.8rem', color: '#928f9b' }}>Other tokens from this address</p>
-        <CousinsList>
-          {this.state.entities.filter((entity) => entity.id !== this.props.entity.id).map(this.renderCousin)}
-        </CousinsList>
+        <CousinsList
+          height={300}
+          width={232.33}
+          rowHeight={74}
+          rowRenderer={this.renderRow}
+          rowCount={this.state.entities.length}
+        />
       </FlatContainer>
     );
   }
 
-  renderCousin = (cousin) => {
+  renderRow = ({ index, key, style }) => {
+    const cousin = this.state.entities[index];
     return (
-      <div key={cousin.id} style={{ display: 'flex', boxAlign: 'center', alignItems: 'center', marginTop: '20px' }}>
+      <div
+        key={key}
+        style={{ display: 'flex', boxAlgin: 'center', alignItems: 'center', paddingTop: '20px', ...style }}
+      >
         <LinkedEntityAvatar id={cousin.id} entityInfo={cousin} size="medium" />
         <Link
           to={`/${cousin.id}`}
