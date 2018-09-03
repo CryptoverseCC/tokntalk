db1f503c5f817cf8b607de21f8f42a96f80307d4
ghostbuster91 <kghost0@gmail.com>
2018-09-03 11:30:14 +0200

    [Refactor] Extract rendering single cousin to function

--- src/CousinsBox.js
+++ src/CousinsBox.js
@@ -42,29 +42,28 @@ export class CousinsBox extends Component {
         <H4 style={{ marginTop: '10px' }}>Cousins</H4>
         <p style={{ fontSize: '0.8rem', color: '#928f9b' }}>Other tokens from this address</p>
         <CousinsList>
-          {this.state.entities.filter((entity) => entity.id !== this.props.entity.id).map((cousin) => {
-            return (
-              <div
-                key={cousin.id}
-                style={{ display: 'flex', boxAlign: 'center', alignItems: 'center', marginTop: '20px' }}
-              >
-                <LinkedEntityAvatar id={cousin.id} entityInfo={cousin} size="medium" />
-                <Link
-                  to={`/${cousin.id}`}
-                  style={{
-                    fontFamily: 'AvenirNext',
-                    fontSize: '1rem',
-                    fontWeight: '600',
-                    marginLeft: '10px',
-                  }}
-                >
-                  {cousin.name}
-                </Link>
-              </div>
-            );
-          })}
+          {this.state.entities.filter((entity) => entity.id !== this.props.entity.id).map(this.renderCousin)}
         </CousinsList>
       </FlatContainer>
     );
   }
+
+  renderCousin = (cousin) => {
+    return (
+      <div key={cousin.id} style={{ display: 'flex', boxAlign: 'center', alignItems: 'center', marginTop: '20px' }}>
+        <LinkedEntityAvatar id={cousin.id} entityInfo={cousin} size="medium" />
+        <Link
+          to={`/${cousin.id}`}
+          style={{
+            fontFamily: 'AvenirNext',
+            fontSize: '1rem',
+            fontWeight: '600',
+            marginLeft: '10px',
+          }}
+        >
+          {cousin.name}
+        </Link>
+      </div>
+    );
+  };
 }
