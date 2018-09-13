4c881c86bf3f39920022b3f25bb180a803e85d70
Karol Kowalski <ordon1313@gmail.com>
2018-09-13 14:30:40 +0200

    Implement personal.sign

--- src/web3.js
+++ src/web3.js
@@ -23,21 +23,17 @@ const setupWeb3 = async () => {
     const web3 = new Web3(window.web3.currentProvider);
     return web3;
   } catch (e) {
+    var keyd = '0xprivatekey';
     const toknTalkEmbeddedWeb3 = {
       eth: {
-        getAccounts: () => new Promise((resolve) => resolve(['0x2823E7756315B3E4FdAac27F5Fe8512294aB4F7f'])),
+        getAccounts: () => new Promise((resolve) => resolve(['0xaDa109225A4A073bAF547fd65c7655c03616A48C'])),
         net: {
           isListening: () => new Promise((resolve) => resolve(true)),
           getId: () => new Promise((resolve) => resolve(1)),
         },
         getBlockNumber: () => new Promise((resolve) => resolve(0)),
         personal: {
-          sign: () =>
-            new Promise((resolve) =>
-              resolve(
-                '0x2f9547b73c9d644159fcbe7af7f5ae19f30f607976e08245f44578a912f4e9c76a5742f7fde3ec7ce7cb17cbc9f210c6789172ece7d2ffe2ad637ea735ed0a131b',
-              ),
-            ),
+          sign: (message, from) => new Web3().eth.accounts.sign(message, keyd).signature,
         },
       },
       currentProvider: {
