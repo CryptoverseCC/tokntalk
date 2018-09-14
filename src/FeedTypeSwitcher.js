7c988f330624cd87a2b1c8f2a1cf90068ab8bff9
Patryk Adaś <patryk.adas@gmail.com>
2018-09-14 12:27:06 +0200

    Tabs main page (#130)

--- src/FeedTypeSwitcher.js
+++ src/FeedTypeSwitcher.js
@@ -6,19 +6,32 @@ const FeedTypeButton = styled.div`
   display: inline;
   font-weight: 600;
   cursor: pointer;
-  font-size: 1.5rem;
+  font-size: 1rem;
   padding: 5px;
+  color: #928f9b;
 
   ${({ selected }) =>
     selected &&
     css`
       color: #1a52ef;
+      text-align: center;
       cursor: default;
-      border-bottom: 3px #1a52ef solid;
+      display: inline-block;
+      :before {
+        content: '';
+        display: block;
+        width: 6px;
+        height: 6px;
+        border-radius: 3px;
+        background-color: #1a52ef;
+        position: relative;
+        bottom: -30px;
+        margin: auto;
+      }
     `};
 
   :not(:first-child) {
-    margin-left: 30px;
+    margin-left: 15px;
   }
 
   @media (max-width: 770px) {
