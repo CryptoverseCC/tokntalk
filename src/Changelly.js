ebd4a3a12d6276c363d30613f10ab6bdd94c4faf
Grzegorz Kapkowski <grzegorz.kapkowski@gmail.com>
2018-10-19 14:08:55 +0200

    Promote supporters

--- src/Changelly.js
+++ src/Changelly.js
@@ -7,28 +7,19 @@ export const ChangellyFastBuy = () => {
     <AppContext>
       {({ web3Store }) => (
         <div>
-          <H4>Ramp.network</H4>
-          <div>Work in progress</div>
-          <br />
-          <H4>Changelly</H4>
-          <div>
-            <span
-              onClick={() =>
-                window.open(
-                  `https://changelly.com/widget/v1?auth=email&from=USD&to=ETH&merchant_id=nsbvzyjs8u57k1lk&address=${
-                    web3Store.from
-                  }&amount=50&ref_id=nsbvzyjs8u57k1lk&color=3a4c44`,
-                  'Changelly',
-                  'width=600,height=470,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=0,left=0,top=0',
-                )
-              }
-            >
-              <img src="https://changelly.com/pay_button.png" alt="Changelly" />
-            </span>
-          </div>
-          <br />
-          <H4>Other</H4>
-          <div>Let us know what other solutions are there at info@cryptoverse.cc or directly on Tok'n'talk</div>
+          <span
+            onClick={() =>
+              window.open(
+                `https://changelly.com/widget/v1?auth=email&from=USD&to=ETH&merchant_id=nsbvzyjs8u57k1lk&address=${
+                  web3Store.from
+                }&amount=50&ref_id=nsbvzyjs8u57k1lk&color=3a4c44`,
+                'Changelly',
+                'width=600,height=470,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=0,left=0,top=0',
+              )
+            }
+          >
+            <img src="https://changelly.com/pay_button.png" alt="Changelly" />
+          </span>
         </div>
       )}
     </AppContext>
